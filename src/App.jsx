de520ea4e9596b53e87396e4ce3c8d5e90e3307f
Salva Castro <salvacastro06@gmail.com>
2026-03-16 22:12:55 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ConstructionPage from './components/ConstructionPage'
+
+function App() {
+  return (
+    <div className="App">
+      <ConstructionPage />
+    </div>
+  )
+}
+
+export default App
